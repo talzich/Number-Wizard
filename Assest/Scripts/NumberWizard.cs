7b143bf43d150aac1ee14e63c183457c92e2cd4b
Tal Zichlinsky <35036457+talzich@users.noreply.github.com>
2020-09-29 12:16:52 +0300

    Add files via upload

---
+++ Assest/Scripts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] Text guessText;
+
+    int mid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        
+        mid = (max + min) / 2;
+        guessText.text = mid.ToString();
+        max++;
+    }
+
+
+    // Update is called once per frame
+
+    public void onPressHigher()
+    {
+        min = mid;
+        nextGuess();
+        EventSystem.current.SetSelectedGameObject(null);
+
+    }
+
+    public void onPressLower()
+    {
+        max = mid;
+        nextGuess();
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    void nextGuess()
+    {
+        mid = (max + min) / 2;
+        guessText.text = mid.ToString();
+    }
+
+    
+}
